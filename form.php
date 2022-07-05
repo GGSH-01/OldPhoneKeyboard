c5ce78c85cbb3192cfdcb39e8ecc985216910559
GGSH-01 <34454560+GGSH-01@users.noreply.github.com>
2022-07-05 15:07:04 +0200

    Update form.php

--- form.php
+++ form.php
@@ -1,5 +1,5 @@
 <form name="mainform" method="POST" action="index.php">
 <p><textarea name="text" cols="60" rows="4"></textarea></p>
-<p><input type="submit" name="submit" value="convert"></p>
+<p><input type="submit" name="submit" value="Convert"></p>
 </form>
 
